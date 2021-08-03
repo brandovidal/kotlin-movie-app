9a3b61824247d425ec10cf9e85ffa400da3ef73b
Brando Vidal <yisusdracon@gmail.com>
2021-08-03 02:02:44 -0500

    feat: Create user imterface 1/3
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

---
+++ app/src/main/java/com/brandovidal/movieapp/MovieFragment.kt
@@ -0,0 +1,11 @@
+package com.brandovidal.movieapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class MovieFragment : Fragment(R.layout.fragment_movie) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
