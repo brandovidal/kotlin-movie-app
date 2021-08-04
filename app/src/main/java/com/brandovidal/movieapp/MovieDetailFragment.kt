1db39833cac00f8daae85c0073ddda975f378e93
Brando Vidal <yisusdracon@gmail.com>
2021-08-04 12:39:25 -0500

    refactor: create interface user 2/3 -> created views and view binding
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- app/src/main/java/com/brandovidal/movieapp/MovieDetailFragment.kt
+++ app/src/main/java/com/brandovidal/movieapp/MovieDetailFragment.kt
@@ -3,9 +3,13 @@ package com.brandovidal.movieapp
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import com.brandovidal.movieapp.databinding.FragmentMovieDetailBinding
 
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
+    private lateinit var binding: FragmentMovieDetailBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieDetailBinding.bind(view)
     }
 }
--- app/src/main/java/com/brandovidal/movieapp/MovieFragment.kt
+++ app/src/main/java/com/brandovidal/movieapp/MovieFragment.kt
@@ -3,9 +3,13 @@ package com.brandovidal.movieapp
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.brandovidal.movieapp.databinding.FragmentMovieBinding
 
 class MovieFragment : Fragment(R.layout.fragment_movie) {
+    private lateinit var binding: FragmentMovieBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieBinding.bind(view)
     }
 }
