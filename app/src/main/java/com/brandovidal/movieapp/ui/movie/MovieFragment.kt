a6e5cb91dbfbd47d484247732d2b5f4d1be620c7
Brando Vidal <yisusdracon@gmail.com>
2021-08-18 03:51:49 -0500

    feat: Add RecylcerView and ConcatAdapter 2/2
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- app/src/main/java/com/brandovidal/movieapp/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/brandovidal/movieapp/ui/movie/MovieFragment.kt
@@ -6,16 +6,23 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ConcatAdapter
 import com.brandovidal.movieapp.R
 import com.brandovidal.movieapp.core.Resource
+import com.brandovidal.movieapp.data.model.Movie
 import com.brandovidal.movieapp.data.remote.MovieDataSource
 import com.brandovidal.movieapp.databinding.FragmentMovieBinding
 import com.brandovidal.movieapp.presentation.MovieViewModel
 import com.brandovidal.movieapp.presentation.MovieViewModelFactory
 import com.brandovidal.movieapp.repository.MovieRepositoryImpl
 import com.brandovidal.movieapp.repository.RetrofitClient
+import com.brandovidal.movieapp.ui.movie.adapters.MovieAdapter
+import com.brandovidal.movieapp.ui.movie.adapters.concat.PopularConcatAdapter
+import com.brandovidal.movieapp.ui.movie.adapters.concat.TopRatedConcatAdapter
+import com.brandovidal.movieapp.ui.movie.adapters.concat.UpcomingConcatAdapter
 
-class MovieFragment : Fragment(R.layout.fragment_movie) {
+class MovieFragment : Fragment(R.layout.fragment_movie), MovieAdapter.OnMovieClickListener {
     private lateinit var binding: FragmentMovieBinding
     private val viewModel by viewModels<MovieViewModel> {
         MovieViewModelFactory(
@@ -25,20 +32,73 @@ class MovieFragment : Fragment(R.layout.fragment_movie) {
         )
     }
 
+    private lateinit var concatAdapter: ConcatAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMovieBinding.bind(view)
 
+        concatAdapter = ConcatAdapter()
+
         viewModel.fetchMainScreenMovies().observe(viewLifecycleOwner, Observer { result ->
-            when(result) {
+            when (result) {
                 is Resource.Loading -> {
-                    Log.d("LiveData", "Loading.... ")
+                    binding.progressBar.visibility = View.VISIBLE
                 }
                 is Resource.Success -> {
-                    Log.d("LiveData", "Upcoming: ${result.data.first} \n \nTopRated ${result.data.second} \n \nPopular ${result.data.third}")}
+                    binding.progressBar.visibility = View.GONE
+                    concatAdapter.apply {
+                        addAdapter(
+                            0,
+                            UpcomingConcatAdapter(
+                                MovieAdapter(
+                                    result.data.first.results,
+                                    this@MovieFragment
+                                )
+                            )
+                        )
+                        addAdapter(
+                            1,
+                            TopRatedConcatAdapter(
+                                MovieAdapter(
+                                    result.data.second.results,
+                                    this@MovieFragment
+                                )
+                            )
+                        )
+                        addAdapter(
+                            2,
+                            PopularConcatAdapter(
+                                MovieAdapter(
+                                    result.data.third.results,
+                                    this@MovieFragment
+                                )
+                            )
+                        )
+                    }
+
+                    binding.rvMovies.adapter = concatAdapter
+                }
                 is Resource.Failure -> {
-                    Log.d("LiveData", "${result.exception} ")}
+                    binding.progressBar.visibility = View.GONE
+                    Log.d("LiveData", "${result.exception} ")
+                }
             }
         })
+    }
+
+    override fun onMovieClick(movie: Movie) {
+        val action = MovieFragmentDirections.actionMovieFragmentToMovieDetailFragment(
+            movie.poster_path,
+            movie.backdrop_path,
+            movie.vote_average.toFloat(),
+            movie.voteCount,
+            movie.overview,
+            movie.title,
+            movie.original_language,
+            movie.release_date
+        )
+
+        findNavController().navigate(action)
     }
 }
--- app/src/main/java/com/brandovidal/movieapp/ui/moviedetails/MovieDetailFragment.kt
+++ app/src/main/java/com/brandovidal/movieapp/ui/moviedetails/MovieDetailFragment.kt
@@ -3,14 +3,28 @@ package com.brandovidal.movieapp.ui.moviedetails
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.navArgs
 import com.brandovidal.movieapp.R
 import com.brandovidal.movieapp.databinding.FragmentMovieDetailBinding
+import com.bumptech.glide.Glide
 
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
     private lateinit var binding: FragmentMovieDetailBinding
+    private val args by navArgs<MovieDetailFragmentArgs>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMovieDetailBinding.bind(view)
+
+        Glide.with(requireContext()).load("https://image.tmdb.org/t/p/w500${args.posterImageUrl}")
+            .centerCrop().into(binding.imgMovie)
+        Glide.with(requireContext())
+            .load("https://image.tmdb.org/t/p/w500${args.backgroundImageUrl}").centerCrop()
+            .into(binding.imgBackground)
+        binding.txtDescription.text = args.overview
+        binding.txtMovieTitle.text = args.title
+        binding.txtLanguage.text = "Language ${args.language}"
+        binding.txtRating.text = "${args.voteAverage} (${args.voteCount} Reviews)"
+        binding.txtReleased.text = "Released ${args.releaseDate}"
     }
 }
