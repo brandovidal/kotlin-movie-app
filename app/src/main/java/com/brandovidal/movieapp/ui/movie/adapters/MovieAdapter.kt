c15f3dfb15dda0b75b1ed4a174d620c9e2675888
Brando Vidal <yisusdracon@gmail.com>
2021-08-17 23:45:22 -0500

    feat: Add RecylcerView and ConcatAdapter 1/2
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

--- app/src/main/java/com/brandovidal/movieapp/ui/movie/adapters/MovieAdapter.kt
+++ app/src/main/java/com/brandovidal/movieapp/ui/movie/adapters/MovieAdapter.kt
@@ -1,20 +1,50 @@
 package com.brandovidal.movieapp.ui.movie.adapters
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.brandovidal.movieapp.core.BaseViewHolder
+import com.brandovidal.movieapp.data.model.Movie
 import com.brandovidal.movieapp.databinding.MovieItemBinding
+import com.bumptech.glide.Glide
 
-class MovieAdapter : RecyclerView.Adapter<BaseViewHolder<*>>() {
+class MovieAdapter(
+    private val moviesList: List<Movie>,
+    private val itemClickListener: OnMovieClickListener
+) : RecyclerView.Adapter<BaseViewHolder<*>>() {
+    interface OnMovieClickListener {
+        fun onMovieClick(movie: Movie)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
-        val itemBinding = MovieItemBinding
+        val itemBinding =
+            MovieItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val holder = MoviesViewHolder(itemBinding, parent.context)
+
+        itemBinding.root.setOnClickListener {
+            val position =
+                holder.bindingAdapterPosition.takeIf { it != DiffUtil.DiffResult.NO_POSITION }
+                    ?: return@setOnClickListener
+            itemClickListener.onMovieClick(moviesList[position])
+        }
+        return holder
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
-        TODO("Not yet implemented")
+        when (holder) {
+            is MoviesViewHolder -> holder.bind(moviesList[position])
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int = moviesList.size
+
+    private inner class MoviesViewHolder(val binding: MovieItemBinding, val context: Context) :
+        BaseViewHolder<Movie>(binding.root) {
+        override fun bind(item: Movie) {
+            Glide.with(context).load("https://image.tmdb.org/t/p/w500${item.poster_path}")
+                .centerCrop().into(binding.imgMovie)
+        }
     }
 }
